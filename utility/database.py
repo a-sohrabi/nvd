d76f480f356ef3579de9d4d8e135b579e47d2b48
a-sohrabi <sohrabi1372@gmail.com>
2024-09-10 12:01:39 +0330

    refact: add creating index functionality

--- utility/database.py
+++ utility/database.py
@@ -5,4 +5,14 @@ from .config import settings
 client = AsyncIOMotorClient(settings.DATABASE_URL)
 database = client[settings.DATABASE_NAME]
 vulnerability_collection = database.get_collection("nvd")
-# vulnerability_collection.create_index('cve_id', unique=True)
+
+
+async def ensure_collection_exists():
+    await vulnerability_collection.insert_one({"_id": "dummy_id"})
+    await vulnerability_collection.delete_one({"_id": "dummy_id"})
+
+
+async def create_indexes():
+    await ensure_collection_exists()
+
+    await vulnerability_collection.create_index("cve_id", unique=True)
